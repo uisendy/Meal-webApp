a0fbfe23426880cea3599bdde9cfff933453a992
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-31 09:36:29 +0100

    update: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
+  entry: "./src/index.js",
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
+      template: "./src/index.html",
       title: "GetSen meal",
     }),
   ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
