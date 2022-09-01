6a1aef710ea15b1d1b608c8f8e658c41de2280b9
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-01 06:24:04 +0100

    [home-page]: Update: Base file to handle API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import "./style.css";
-import Header from "./components/header.js";
-import HeroSection from "./components/HeroSection.js";
-import MealDisplay from "./components/MealDisplay.js";
+import Header from "./modules/header.js";
+import HeroSection from "./modules/HeroSection.js";
+import showMealUI from "./modules/ShowMealUI.js";
+import InvolvementAPI from "./modules/involvementAPI";
+import MealAPI from "./modules/mealAPI";
 
 const main = document.querySelector(".main-container");
 
-console.log(Header());
+const handleLikes = () => {};
 
-document.body.appendChild(Header());
-main.appendChild(HeroSection());
-main.appendChild(MealDisplay());
-document.body.appendChild(main);
+window.addEventListener("load", async () => {
+  document.body.appendChild(Header());
+  main.appendChild(await HeroSection());
+  main.appendChild(await showMealUI.MealDisplay());
+  document.body.appendChild(main);
+
+  const mealSection = document.querySelector(".meal-section");
+  mealSection.addEventListener("click", async (e) => {
+    if (e.target.classList.contains("meal-cat")) {
+      const category = e.target.textContent;
+      let cardContainer = document.querySelector(".card-container");
+      let mealContainer = document.querySelector(".meal-card");
+      mealContainer.remove();
+      cardContainer.appendChild(await showMealUI.createCard(category));
+    }
+
+    if (e.target.classList.contains("fa-heart")) {
+      showMealUI.handleLikeUI(e);
+
+      const mealName =
+        e.target.parentElement.parentElement.parentElement.firstElementChild
+          .firstElementChild.textContent;
+      await InvolvementAPI.addLike(mealName);
+    }
+  });
+});
