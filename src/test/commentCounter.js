e1b4dec68fd926b75246a04e1b57cf8c86b37bb1
Surafel Getachew <suragetch@gmail.com>
2022-09-01 16:54:42 +0300

    Create test to count 1 comment

--- src/test/commentCounter.js
+++ src/test/commentCounter.js
@@ -23,4 +23,11 @@ describe('Counting number of comments', () => {
 
     expect(num).toEqual(0);
   });
+  
+  it('Add 1 comment', () => {
+    const [container1] = createEnv();
+    const num = PopUp.commentCountAdd(container1);
+
+    expect(num).toEqual(1);
+  });
 });
